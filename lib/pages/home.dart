5d7334ca2bf6252a3870274b522c87fd3e7edd4a
Rodolfo Schmitt <Rodo-Hans@Hotmail.com>
2020-01-21 23:18:03 -0600

    Created home page with app bar and bab.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
 
 class Home extends StatelessWidget {
   @override
@@ -10,6 +12,7 @@ class Home extends StatelessWidget {
           color: Colors.lightGreen.shade900,
           ),
         ),
+        automaticallyImplyLeading: false,
         elevation: 0.0,
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(32.0),
@@ -21,6 +24,7 @@ class Home extends StatelessWidget {
               colors: [Colors.lightGreen, Colors.lightGreen.shade50],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
+              transform: GradientRotation(math.pi/3),
             ),
           ),
         ),
@@ -33,7 +37,29 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      bottomNavigationBar: ,
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0.0,
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 44.0,
+          /*decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.lightGreen.shade200, Colors.lightGreen],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            )
+          ),*/
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //TODO add navigator to edit/add journal entry page
+        },
+        tooltip: 'Add/edit entry',
+        backgroundColor: Colors.lightGreen.shade300,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
